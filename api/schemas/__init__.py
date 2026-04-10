a4730f83a4981d94aa3bc542758cdbc55837fc09
joao5142 <joaopauloneto3687@gmail.com>
2026-04-09 22:49:59 -0300

    refactor: adjust schema file

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -1,2 +1,10 @@
 from schemas.error_schema import ErrorSchema
-                                    
+from schemas.water_schema import (
+    WaterSchema,
+    WaterViewSchema,
+    WaterListSchema,
+    WaterSearchSchema,
+    WaterDelSchema,
+    apresenta_water,
+    apresenta_waters,
+)
